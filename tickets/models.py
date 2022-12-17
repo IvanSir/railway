e020f87e38124919a09b7de4c23f1f91c0f1f4e7
IvanSir <ginvaelwf@mail.ru>
2022-12-17 21:53:27 +0300

    Revert "admin str fix"
    This reverts commit 31578b36a866207361df9cdce067857079c7fcc5.

--- tickets/models.py
+++ tickets/models.py
@@ -10,7 +10,7 @@ class Ticket(models.Model):
     arrival_point = models.ForeignKey('tickets.ArrivalPoint', on_delete=models.CASCADE, related_name="tickets")
 
     def __str__(self):
-        return f'{self.id} - ({self.departure_point}:{self.arrival_point}) - seat:{self.seat_number}, price:{self.price}'
+        return f'{self.carriage}, Seat: {self.seat_number}'
 
 
 class ArrivalPoint(models.Model):
@@ -18,7 +18,7 @@ class ArrivalPoint(models.Model):
     arrival_place = models.CharField(max_length=255)
 
     def __str__(self):
-        return f'{self.arrival_city.city_name} - {self.arrival_place}'
+        return str(self.id)
 
 
 class Route(models.Model):
@@ -26,7 +26,7 @@ class Route(models.Model):
     departure_time = models.DateTimeField(blank=False, null=False)
 
     def __str__(self):
-        return f'{self.id} - From {self.departure_city} at {self.departure_time.strftime("%Y-%m-%d %H:%M")}'
+        return f'From {self.departure_city} at {self.departure_time}'
 
 
 class RouteToArrivalPoint(models.Model):
@@ -35,9 +35,6 @@ class RouteToArrivalPoint(models.Model):
     order = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     arrival_time = models.DateTimeField()
-
-    def __str__(self):
-        return f'{self.id} - {self.route.departure_city} to {self.arrival_point} at {self.arrival_time.strftime("%Y-%m-%d %H:%M")} for {self.price} (order - {self.order})'
 
 
 class CarriageType(models.Model):
@@ -49,9 +46,6 @@ class CarriageType(models.Model):
     carriage_type_name = models.CharField(max_length=25, choices=CARRIAGE_CHOICES)
     description = models.TextField(max_length=255, null=True, blank=True)
 
-    def __str__(self):
-        return f'{self.id} - {self.carriage_type_name}'
-
 
 class Carriage(models.Model):
     carriage_type = models.ForeignKey('tickets.CarriageType', on_delete=models.CASCADE)
@@ -59,7 +53,7 @@ class Carriage(models.Model):
     route = models.ForeignKey('tickets.Route', on_delete=models.CASCADE, related_name='carriages')
 
     def __str__(self):
-        return f'{self.id} - {self.carriage_type.carriage_type_name}, seats - {self.seat_amount}'
+        return f'{self.carriage_type}: {self.id}'
 
 
 class Order(models.Model):
@@ -72,13 +66,10 @@ class Order(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='orders')
 
-    def __str__(self):
-        return f'{self.id} - {self.user.username}/{self.total_price}/{self.order_status}'
-
 
 class City(models.Model):
     city_name = models.CharField(max_length=32, unique=True)
     description = models.TextField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.id} - {self.city_name}'
+        return self.city_name
